747d908cd998595abb410ad04b66c3970f98e05b
semidragon7 <semidragon@interia.pl>
2016-07-07 08:56:04 +0200

    I finished

--- app/datasets.js
+++ app/datasets.js
@@ -4,60 +4,61 @@ module.exports = {
 
     clamp1: function ()
     {
-        return [];
+        return [3,6];
     },
 
     clamp2: function ()
     {
-        return [];
+        return [9,2,7];
     },
 
     clamp3: function ()
     {
-        return [];
+        return [5,0,10];
     },
 
     inRange1: function ()
     {
-        return [];
+        return [2,1,3];
     },
 
     inRange2: function ()
     {
-        return [];
+        return [4,7,5];
     },
 
     inRange3: function ()
     {
-        return [];
+        return [8,2,5];
     },
 
     inRange4: function ()
     {
-        return [];
+        return [2,4];
     },
 
     inRange5: function ()
     {
-        return [];
+        return [-1,4];
     },
 
     inRange6: function ()
     {
-        return [];
+        return [9,6];
     },
 
     inRange7: function ()
     {
-        return [];
+        return [3,3];
     },
 
     random1: function ()
+
     {
-        return [];
+        return [3,9,true];
     },
     random2: function ()
     {
-        return [];
+        return [1,5];
     }
 };
